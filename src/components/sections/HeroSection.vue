5bdcc4e357b31e8f227b5adc4006817467ba144f
Onda-dev <manuelaltieri3@gmail.com>
2022-06-15 18:54:14 +0200

    hero fix

--- src/components/sections/HeroSection.vue
+++ src/components/sections/HeroSection.vue
@@ -3,9 +3,9 @@
         <div class="hero d-flex justify-content-between align-items-center">
             <div class="text-box">
                 <p>17 years of experience</p>
-                <h1>Focus on your experience</h1>
+                <h1>Focus on your <br> experience</h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non quas atque possimus, libero eum laborum velit illo provident quisquam vitae eligendi officiis enim hic excepturi doloribus consectetur voluptas dolore veritatis?</p>
-                <button type="button" class="grey-button">
+                <button type="button" class="grey-button mt-5">
                     read more
                 </button>
                 <div class="social-row">
@@ -64,7 +64,7 @@ export default {
         gap: 1.875rem;
     }
     .social-row {
-        padding-top: 8rem;
+        padding-top: 6rem;
     }
 }
 </style>
