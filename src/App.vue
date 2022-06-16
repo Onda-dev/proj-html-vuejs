66483fa4b520d6ca07cc798af4e7916848fd4fb0
Onda-dev <manuelaltieri3@gmail.com>
2022-06-16 18:44:44 +0200

    Footer top inserito

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,21 @@
   <div id="app">
     <HeaderBase/>
     <MainBase/>
-
+    <FooterBase/>
   </div>
 </template>
 
 <script>
 import HeaderBase from './components/blocks/HeaderBase.vue'
 import MainBase from './components/blocks/MainBase.vue'
+import FooterBase from './components/blocks/FooterBase.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderBase,
     MainBase,
+    FooterBase
   }
 }
 </script>
---
+++ src/components/blocks/FooterBase.vue
@@ -0,0 +1,82 @@
+<template>
+    <footer>
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <div class="col-3">
+                        <img src="@/assets/img/logo.png" alt="">
+                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto mollitia tenetur nihil, beatae veritatis sapiente optio cum culpa provident ullam quod ea ex consectetur quasi aliquam eaque accusantium incidunt commodi.</p>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <h4>quick links</h4>
+                            <nav>
+                                <ul class="text-center">
+                                    <li v-for="(link, index) in quickLinks" :key="index">
+                                        <a :href="link.link">{{link.text}}</a>
+                                    </li>
+                                </ul>
+                            </nav>
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <h4>contact</h4>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+                            <p>info@yourdomain.com</p>
+                            <p>+99 (0) 101 0000 888</p>
+                        </a>
+                    </div>
+                    <div class="col-3">
+                        <a href="#">
+                            <h4>locations on map</h4>
+                            <img src="@/assets/img/map.png" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: 'FooterBase',
+    data() {
+        return {
+            quickLinks: [
+                {
+                    "text": "Careers",
+                    "link": "#",
+                },
+                {
+                    "text": "News",
+                    "link": "#",
+                },
+                {
+                    "text": "Terms of Use",
+                    "link": "#",
+                },
+                {
+                    "text": "Privacy Projects",
+                    "link": "#",
+                },
+                {
+                    "text": "About",
+                    "link": "#",
+                },
+                {
+                    "text": "Contact",
+                    "link": "#",
+                },
+            ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+img {
+    width: 100%;
+}
+</style>
