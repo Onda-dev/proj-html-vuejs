d61a01522ede2220fc24def217bab951c38392f5
Onda-dev <manuelaltieri3@gmail.com>
2022-06-16 18:26:48 +0200

    Aggiunte schede sezione blog

--- src/components/sections/BlogSection.vue
+++ src/components/sections/BlogSection.vue
@@ -5,12 +5,24 @@
             <div class="row">
                 <div class="col-4">
                     <div class="img-box first-img-box"></div>
+                    <div class="info-box">
+                        <div class="date">May 24, 2018 - by Alex</div>
+                        <div class="title"><i class="fa-solid fa-circle"></i> Marketing Ideas</div>
+                    </div>
                 </div>
                 <div class="col-4">
-                    <div class="img-box second-img-box"></div>                    
+                    <div class="img-box second-img-box"></div>
+                    <div class="info-box">
+                        <div class="date">May 24, 2018 - by Alex</div>
+                        <div class="title"><i class="fa-solid fa-circle"></i> Marketing Ideas</div>
+                    </div>               
                 </div>
                 <div class="col-4">
-                    <div class="img-box third-img-box"></div>                    
+                    <div class="img-box third-img-box"></div>
+                    <div class="info-box">
+                        <div class="date">May 24, 2018 - by Alex</div>
+                        <div class="title"><i class="fa-solid fa-circle"></i> Marketing Ideas</div>
+                    </div>                    
                 </div>
             </div>
             <button type="button" class="grey-button mt-5">
@@ -29,6 +41,7 @@ export default {
 <style lang="scss" scoped>
 section {
     margin: 6.25rem 0;
+    background-color: rgb(231, 231, 231);
 }
 .first-img-box {
     background-image: url(/src/assets/img/serious-businesswoman-with-documents-talking-on-P9Q6LX6-1024.jpg);
@@ -43,5 +56,20 @@ section {
     height: 16rem;
     background-size: cover;
     border-radius: .625rem;
+}
+.info-box {
+    transform: translate(0, -50%);
+    margin: auto;
+    padding: 1.875rem;
+    border-radius: .625rem;
+    text-align: left;
+    width: 85%;
+    background-color: rgb(255, 255, 255);
+    i {
+        font-size: .4rem;
+        color: lime;
+        display: inline-block;
+        transform: translate(0, -50%);
+    }
 }
 </style>
